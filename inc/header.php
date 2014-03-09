5d784b94cb6a423c7297253f8c46ee272f48e209
Justin Cano <jcano001@ucr.edu>
2014-03-09 04:19:01 -0700

    header and footer added

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>UCR BCOE GPA Calculator</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Justin Cano">
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="https://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
+<?php include 'inc/header.php'; ?>
+
+
+	<form id="myForm" action="#" method="post" role="form">
+		<div class="form-group col-md-3">
+			Letter Grade
+		</div>
+		<div class="form-gropu col-md-3">
+			Units
+		</div>
+		<button type="submit">Submit</button>
+		</div>
+	</form>
+
+	<button id="add">+</button>
 
 
 
@@ -7,7 +21,4 @@
 
 
 
-
-
-
-?>
+<?php include 'inc/footer.php'; ?>
