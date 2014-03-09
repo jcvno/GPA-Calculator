faa4b3f8d0f56ab86bff7bbb578526a2e5b2407a
Justin Cano <jcano001@ucr.edu>
2014-03-09 13:10:06 -0700

    added value and unit value form inputs

--- index.php
+++ index.php
@@ -4,9 +4,33 @@
 		<form id="myForm" action="#" method="post" role="form">
 			<div class="form-group col-md-3">
 				Letter Grade
+				<br>
+				<select class="form-control" name="value">
+					<option selected="selected" value="A">A/A+</option>
+					<option value="A-">A-</option>
+					<option value="B+">B+</option>
+					<option value="B">B</option>
+					<option value="B-">B-</option>
+					<option value="C+">C+</option>
+					<option value="C">C</option>
+					<option value="C-">C-</option>
+					<option value="D+">D+</option>
+					<option value="D">D</option>
+					<option value="D-">D-</option>
+					<option value="F">F</option>
+				</select>
 			</div>
 			<div class="form-group col-md-3">
 				Units
+				<br>
+				<select class="form-control" name="unit-value">
+					<option selected="selected" value="1">1.0</option>
+					<option value="2">2.0</option>
+					<option value="3">3.0</option>
+					<option value="4">4.0</option>
+					<option value="5">5.0</option>
+					<option value="6">6.0</option>
+				</select>
 			</div>
 			<br>
 			<button type="submit">Submit</button>
