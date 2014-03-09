f2db3bd3fcfd7da737c61c738844e2f9afbce96a
Justin Cano <jcano001@ucr.edu>
2014-03-09 04:23:49 -0700

    container added

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php include 'inc/header.php'; ?>
 
+	<div class="container">
+		<form id="myForm" action="#" method="post" role="form">
+			<div class="form-group col-md-3">
+				Letter Grade
+			</div>
+			<div class="form-group col-md-3">
+				Units
+			</div>
+			<div class="col-md-3 col-md-offset-9">
+				<button type="submit">Submit</button>
+			</div>
+		</form>
 
-	<form id="myForm" action="#" method="post" role="form">
-		<div class="form-group col-md-3">
-			Letter Grade
-		</div>
-		<div class="form-group col-md-3">
-			Units
-		</div>
-		<div class="col-md-3 col-md-offset-9">
-			<button type="submit">Submit</button>
-		</div>
-	</form>
+		<br>
 
-	<button id="add">+</button>
-
+		<div class="col-md-1 col-mid-offset-3">
+			<button id="add">+</button>
+		</div>
+	</div>
 
 
 
