db6e173608704fd01d6e0a9ce877a31a72640ae8
Justin Cano <justin.michael.cano@gmail.com>
2014-03-09 17:26:28 -0700

    GPA calculation added

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 		<div class="row">
 			<p id="total"><span class="col-md-3">Total Units:</span> <span id="QHRS" class="col-md-1 col-md-offset-2">1</span></p>
 			<br>
+			<p><strong>GPA:</strong> <span id="GPA"></span></p>
+			<br>
 		</div>
 		<div class="row">
 			<div class="col-md-1 col-md-offset-3">
@@ -18,7 +20,7 @@
 	<script>
 		var input = "<div class='row input'><div class='form-group col-md-3 letter'>Letter Grade<br><select class='form-control letter-value'><option selected='selected' value='4.0'>A/A+</option><option value='3.7'>A-</option><option value='3.3'>B+</option><option value='3.0'>B</option><option value='2.7'>B-</option><option value='2.3'>C+</option><option value='2.0'>C</option><option value='1.7'>C-</option><option value='1.3'>D+</option><option value='1.0'>D</option><option value='0.7'>D-</option><option value='0.0'>F</option></select></div><div class='form-group col-md-3 units'>Units<br><select class='form-control unit-value'><option selected='selected' value='1.0'>1.0</option><option value='2.0'>2.0</option><option value='3.0'>3.0</option><option value='4.0'>4.0</option><option value='5.0'>5.0</option><option value='6.0'>6.0</option></select></div></div>";
 
-		var QHRS = 0;
+		var QHRS = 1;
 		function sumUnits(){
 			QHRS = 0;
 			$(".unit-value").each(function(){
@@ -28,17 +30,25 @@
 			document.getElementById("QHRS").innerHTML=QHRS;
 		}
 
-		var gradePoints = 0;
+		var QPTS = 0;
 		function calculateGradePoints(){
-			gradePoints = 0;
+			QPTS = 0;
 			$(".input").each(function(){
 				var value = (+$(this).find(".letter").find(".letter-value").val());
 				var unitValue = (+$(this).find(".units").find(".unit-value").val());
-				gradePoints = gradePoints + (value*unitValue);
+				QPTS = QPTS + (value*unitValue);
 			});
 		}
 
+		var GPA = 0;
+		function calculateGPA(){
+			GPA = QPTS/QHRS
+			document.getElementById("GPA").innerHTML=GPA;
+		}
+
 		$("#total").before(input);
+		sumUnits();
+		calculateGradePoints();
 		$(".unit-value").change(sumUnits);
 		$(".letter-value").change(calculateGradePoints);
 		$("#add").click(function(){
@@ -51,7 +61,7 @@
 			$("#total").prev().remove();
 			sumUnits();
 		});
-
+		$("#submit").click(calculateGPA);
 
 	</script>
 
